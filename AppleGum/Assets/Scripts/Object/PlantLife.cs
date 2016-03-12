3e0bf09226034997cc6091d1781825cb87ba1eba
Jonathan Rompré <romprejonathan@gmail.com>
2016-03-12 18:21:15 -0500

    Fixes and plant working

---
+++ AppleGum/Assets/Scripts/Object/PlantLife.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlantLife : MonoBehaviour {
+
+	//variables
+	//---------
+	private Animator _animator;
+	private float timer = 0;
+
+	public static int _plantstate = 1;
+	public static bool _fullgrow = false;
+
+
+
+	void Start () {
+		_animator = gameObject.GetComponent<Animator> ();
+		if (_fullgrow) _plantstate = 5;
+	}
+
+
+	void Update () {
+
+		if (!_fullgrow) timer -= Time.deltaTime;
+
+		if (timer < -2) { _plantstate++ ; timer = 0; } // Grow interval
+
+		if (_plantstate > 4) _fullgrow = true; //Declare when plant is full gown
+
+
+		_animator.SetBool ("FullGrown", _fullgrow);
+		_animator.SetInteger("PlantState",_plantstate);
+	}
+}
---
+++ AppleGum/Assets/Scripts/System/GroundCheck.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+
+
+	private Player Player;
+
+	// Use this for initialization
+	void Start () {
+
+		Player = gameObject.GetComponentInParent<Player> ();
+	
+	}
+
+
+	void OnTriggerEnter2D (Collider2D col){
+
+		Player.grounded = true;
+	}
+
+	void OnTriggerExit2D (Collider2D col){
+
+		Player.grounded = false;
+	}
+
+	void OnTriggerStay2D (Collider2D col){
+
+		Player.grounded = true;
+	}
+
+
+}
