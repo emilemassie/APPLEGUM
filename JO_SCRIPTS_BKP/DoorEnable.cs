452ccc80e60b9975dd7d6b7c50ecee3a1bfc9214
Jonathan Rompré <romprejonathan@gmail.com>
2016-03-11 20:59:01 -0500

    no message

---
+++ JO_SCRIPTS_BKP/DoorEnable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorEnable : MonoBehaviour {
+
+    private Animator Door;
+    public static bool Door_En = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Door = gameObject.GetComponent<Animator>();
+        Door_En = SwitchEnable.Switch_En;
+        Door_En = false;
+        Door.SetBool("Door_En", Door_En);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Door_En = SwitchEnable.Switch_En;
+        Door.SetBool("Door_En", Door_En);
+    }
+}
